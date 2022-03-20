c82e507a9ae25ece0d4d6a72dde903b53db4331c
Vijayasai S <vijayasai10@gmail.com>
2022-03-20 12:59:30 +0530

    alphaserver

---
+++ alphaserver/constants.py
@@ -0,0 +1,8 @@
+
+
+
+# This is the inventory which contains the URLs for hitting the alphaclient's wsgi application for ssh login attempts
+# We are using a map where the KEY is HOST and VALUE is BASE URL.
+URL = {
+    "ABC":"http://127.0.0.1:5000/"
+}
--- alphaserver/run.py
+++ alphaserver/run.py
@@ -1,11 +1,31 @@
-
-from flask import Flask
-
-
-app = Flask(__name__)
+from fastapi import FastAPI
+import aiohttp
+import constants
 
 
+app = FastAPI()
+
+@app.get("/sshcount/{host}/{date}")
+async def get_ssh_count(host,date):
+    # Please pass the date as string in YYYY-MM-DD
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(constants.URL[host]+"?date="+date) as response:
+            ssh_attempt_count = await response.text()
+    return {"ssh_login_attempt": int(ssh_attempt_count), "date":date, "host": host}
 
 
-if __name__ == "__main__":
-    app.run()
+
+@app.get("/sshcount/all/{date}")
+async def get_ssh_count(date):
+    # Please pass the date as string in YYYY-MM-DD
+
+    all_hosts_ssh_attempts = {}
+    for host in constants.URL:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(constants.URL[host]+"?date="+date) as response:
+                ssh_attempt_count = await response.text()
+                all_hosts_ssh_attempts[host] = ssh_attempt_count
+
+    return {"date":date,"all_ssh_login_attempts": all_hosts_ssh_attempts}
+
